ed33bf6ae51cea80f346676cda6fdbf66cf3213d
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-03-28 15:38:47 -0700

    Successfully displayed table with narrowed results. Simple version complete

--- static/js/app.js
+++ static/js/app.js
@@ -3,23 +3,37 @@ let ufoData = data,
     button = d3.select("#filter-btn"),
     form = d3.select(".filters");
 
+const tbody = d3.select("tbody");
+
 // Event handler function for the form
 const runEnter = () => {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
+    //Clear tbody html in index.html
+    tbody.html("");
+    
     //Grab date from the form
     let dateElement = d3.select("#datetime"),
         dateValue = dateElement.property("value");
 
     console.log(dateValue)
 
+    //filter date down to user specified date
     let filteredData = ufoData.filter(sighting => sighting.datetime === dateValue);
 
     console.log(filteredData);
 
-}
+    //Append filtered data to table in the html code
+    filteredData.forEach((sighting) => {
+        let row = tbody.append("tr");
+        Object.values(sighting).forEach(value => {
+            let cell = row.append("td");
+            cell.text(value); 
+        });
+     });
+};
 
 button.on("click", runEnter);
 form.on("submit",runEnter);
