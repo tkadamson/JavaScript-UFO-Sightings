dea23c8dddf567b0da627c5b6e750c91fbe076ed
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-03-28 15:05:28 -0700

    Created function that runs on button click. Succesfully console logged the filtered data.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,25 @@
-// from data.js
-var tableData = data;
+// Assign data.js to a variable, and select the button and form
+let ufoData = data,
+    button = d3.select("#filter-btn"),
+    form = d3.select(".filters");
 
-// YOUR CODE HERE!
+// Event handler function for the form
+const runEnter = () => {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    //Grab date from the form
+    let dateElement = d3.select("#datetime"),
+        dateValue = dateElement.property("value");
+
+    console.log(dateValue)
+
+    let filteredData = ufoData.filter(sighting => sighting.datetime === dateValue);
+
+    console.log(filteredData);
+
+}
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
