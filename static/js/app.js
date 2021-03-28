7b8abe6f648f9d59e1cb90e94434e78036f696e9
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-03-28 16:25:02 -0700

    Added a form element for each of the extended criteria. Added if statements so if the value in the cell is not black, the filter operates on that element. Coding completed for this project

--- static/js/app.js
+++ static/js/app.js
@@ -13,26 +13,67 @@ const runEnter = () => {
 
     //Clear tbody html in index.html
     tbody.html("");
-    
-    //Grab date from the form
+
+    //Grab all elements and values from the form
     let dateElement = d3.select("#datetime"),
         dateValue = dateElement.property("value");
 
-    console.log(dateValue)
+    console.log(dateValue);
 
-    //filter date down to user specified date
-    let filteredData = ufoData.filter(sighting => sighting.datetime === dateValue);
+    let cityElement = d3.select("#city"),
+        cityValue = cityElement.property("value");
 
-    console.log(filteredData);
+    console.log(cityValue);
+
+    let stateElement = d3.select("#state"),
+        stateValue = stateElement.property("value");
+
+    console.log(stateValue);
+
+    let countryElement = d3.select("#country"),
+        countryValue = countryElement.property("value");
+
+    console.log(countryValue);
+
+    let shapeElement = d3.select("#shape"),
+        shapeValue = shapeElement.property("value");
+
+    console.log(shapeValue)
+
+    //filter date down to user specified metrics
+    if (dateValue !== "") {
+        ufoData = ufoData.filter(sighting => sighting.datetime === dateValue);
+    }
+
+    if (cityValue !== "") {
+        ufoData = ufoData.filter(sighting => sighting.city === cityValue);
+    }
+
+    if (stateValue !== "") {
+        ufoData = ufoData.filter(sighting => sighting.state === stateValue);
+    }
+
+    if (countryValue !== "") {
+        ufoData = ufoData.filter(sighting => sighting.country === countryValue);
+    }
+
+    if (shapeValue !== "") {
+        ufoData = ufoData.filter(sighting => sighting.shape === shapeValue);
+    }
+
+    console.log(ufoData);
 
     //Append filtered data to table in the html code
-    filteredData.forEach((sighting) => {
+    ufoData.forEach((sighting) => {
         let row = tbody.append("tr");
         Object.values(sighting).forEach(value => {
             let cell = row.append("td");
             cell.text(value); 
         });
      });
+     
+    //Reset ufoData back to original data set for next search
+    ufoData = data
 };
 
 button.on("click", runEnter);
